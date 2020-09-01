d47a7987e5faa6c151444804cceeae07541dd37b
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-01 23:17:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,11 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.collect do |deli|
-    "The line is currently"
-  end
+    queue = []
+    katz_deli.each do |deli|
+      queue << "#{katz_deli.index(deli) + 1}. #{deli}"
+    end
+    puts "The line is currently: " + queue.join(" ")
   end
 end
 
