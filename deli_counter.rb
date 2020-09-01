6b83f25b6a295e192cccf1238a26d0fbfb40953b
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-01 23:14:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.collect{|deli| "#{deli.index + 1}. #{deli}"}
-    puts "The line is currently: " + katz_deli.join(" ")
   end
 end
 
